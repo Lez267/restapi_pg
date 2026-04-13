156919e96d130e56e9a7e1abfbc38da4980f15aa
Lezz <leagalejandro@gmail.com>
2026-04-13 07:35:19 -0600

    restapi actualizado

--- callrestapi.js
+++ callrestapi.js
@@ -1,31 +1,50 @@
-var url= "http://localhost:3300/api/users";
-function postUser(){
-console.log(url);
+var url = "http://localhost:3300/api/users";
 
-var myName = $('#name').val();
-var myEmail= $('#email').val();
-var myAge =  $('#age').val();
-var myComments = $('#comments').val();
+function postUser() {
+    console.log(url);
+
+    var myName = $('#name').val();
+    var myEmail = $('#email').val();
+    var myAge = $('#age').val();
+    var myComments = $('#comments').val();
+
+    var myuser = {
+        name: myName,
+        email: myEmail,
+        age: myAge,
+        comments: myComments
+    };
+
+    console.log(myuser);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            console.log(data);
+            $('#resultado').html(JSON.stringify(data.user));
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        },
+        data: JSON.stringify(myuser)
+    });
+}
 
 
-var myuser ={
-name : myName,
-email : myEmail,
-age : myAge,
-comments : myComments
-};
-
-console.log(myuser);
-
-$.ajax({
-url : url,
-type : 'post',
-dataType : 'json',
-contentType : 'application/json',
-succes : function (data){
-console.log(data);
-$('#resultado').html(JSON.stringify(data.user));
-},
-data : JSON.stringify(myuser)
-});
+function getUsers() {
+    $.ajax({
+        url: "http://localhost:3300/api/users",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            $('#resultado').html(JSON.stringify(data));
+        },
+        error: function (error) {
+            console.error("Error:", error);
+        }
+    });
 }
